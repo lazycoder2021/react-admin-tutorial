819528132a241fa07d30fb33cbe8f5139409b546
lazycoder2021 <tksingh2087@gmail.com>
2021-02-19 23:35:56 +0530

    Initial Comitt

---
+++ client/src/components/UserEdit.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Edit, SimpleForm, TextInput } from 'react-admin';
+
+const UserEdit = (props) => {
+    return (
+        <Edit title="Edit User" {...props}>
+            <SimpleForm>
+                <TextInput disabled source='id' />
+                <TextInput source='name' />
+                <TextInput source='email' />
+            </SimpleForm>
+        </Edit>
+    )
+
+}
+
+export default UserEdit
+
+
+
+
+
+
+// JavaScript source code
+// JavaScript source code
+
+
+
+
+
+
+
+
+
+
+
+
+// JavaScript source code
---
+++ range.js
@@ -0,0 +1,22 @@
+module.exports = (req, res, next) => {
+    res.header('Content-Range', 'posts 0-20/20')
+    next()
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// JavaScript source code
